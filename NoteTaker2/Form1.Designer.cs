acb07d550d5c2bbc141d99c19da34243017ae667
Baha3035 <batmaaanpls@gmail.com>
2023-12-11 18:04:08 +0600

    initial commit

---
+++ NoteTaker2/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace NoteTaker2
+{
+    partial class NoteTaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.previousNotes = new System.Windows.Forms.DataGridView();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.newNoteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(461, 28);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(327, 22);
+            this.titleBox.TabIndex = 0;
+            this.titleBox.TextChanged += new System.EventHandler(this.titleBox_TextChanged);
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(461, 79);
+            this.noteBox.Multiline = true;
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(327, 359);
+            this.noteBox.TabIndex = 1;
+            this.noteBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(458, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Title:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(458, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(330, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Note:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // previousNotes
+            // 
+            this.previousNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.previousNotes.BackgroundColor = System.Drawing.Color.White;
+            this.previousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.previousNotes.Location = new System.Drawing.Point(12, 67);
+            this.previousNotes.Name = "previousNotes";
+            this.previousNotes.RowHeadersWidth = 51;
+            this.previousNotes.RowTemplate.Height = 24;
+            this.previousNotes.Size = new System.Drawing.Size(440, 232);
+            this.previousNotes.TabIndex = 4;
+            this.previousNotes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.previousNotes.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.previousNotes_CellDoubleClick);
+            // 
+            // loadButton
+            // 
+            this.loadButton.BackColor = System.Drawing.Color.White;
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadButton.Location = new System.Drawing.Point(13, 319);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(132, 34);
+            this.loadButton.TabIndex = 5;
+            this.loadButton.Text = "load";
+            this.loadButton.UseVisualStyleBackColor = false;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.White;
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.Location = new System.Drawing.Point(320, 319);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(132, 34);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // newNoteButton
+            // 
+            this.newNoteButton.BackColor = System.Drawing.Color.White;
+            this.newNoteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.newNoteButton.Location = new System.Drawing.Point(12, 368);
+            this.newNoteButton.Name = "newNoteButton";
+            this.newNoteButton.Size = new System.Drawing.Size(132, 34);
+            this.newNoteButton.TabIndex = 7;
+            this.newNoteButton.Text = "new note";
+            this.newNoteButton.UseVisualStyleBackColor = false;
+            this.newNoteButton.Click += new System.EventHandler(this.newNoteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.White;
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.Location = new System.Drawing.Point(320, 368);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(132, 34);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(12, 28);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(440, 22);
+            this.searchBox.TabIndex = 9;
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.searchLabel.Location = new System.Drawing.Point(9, 2);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(330, 23);
+            this.searchLabel.TabIndex = 10;
+            this.searchLabel.Text = "Search:";
+            this.searchLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.searchLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // NoteTaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.newNoteButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.previousNotes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.titleBox);
+            this.Name = "NoteTaker";
+            this.Text = "NoteTaker";
+            this.Load += new System.EventHandler(this.NoteTaker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.TextBox noteBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView previousNotes;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button newNoteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label searchLabel;
+    }
+}
+
---
+++ NoteTaker2/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteTaker2
+{
+    public partial class NoteTaker : Form
+    {
+        private SQLiteConnection sqliteConn;
+        private SQLiteDataAdapter sqliteDataAdapter;
+        DataTable notes;
+        bool editing = false;
+        public NoteTaker()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            // Connection string for SQLite database file (modify as needed)
+            string connectionString = "Data Source=notes.db;Version=3;";
+
+            // Create a new SQLite database connection
+            sqliteConn = new SQLiteConnection(connectionString);
+
+            // Create the notes table if it doesn't exist
+            using (SQLiteCommand command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Notes (Title TEXT, Note TEXT)", sqliteConn))
+            {
+                sqliteConn.Open();
+                command.ExecuteNonQuery();
+            }
+
+            // Load data from the database into the DataTable
+            notes = new DataTable();
+            sqliteDataAdapter = new SQLiteDataAdapter("SELECT * FROM Notes", sqliteConn);
+            sqliteDataAdapter.Fill(notes);
+
+            // Set the DataTable as the DataSource for the DataGridView
+            previousNotes.DataSource = notes;
+
+            sqliteConn.Close();
+        }
+
+        private void NoteTaker_Load(object sender, EventArgs e)
+        {
+            //notes.Columns.Add("Title");
+            //notes.Columns.Add("Note");
+
+            previousNotes.DataSource = notes;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
+        }
+
+        private void newNoteButton_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = "";
+            noteBox.Text = "";
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try {
+                notes.Rows[previousNotes.CurrentCell.RowIndex].Delete();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Not a valid note");
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (editing)
+                {
+                    notes.Rows[previousNotes.CurrentCell.RowIndex]["Title"] = titleBox.Text;
+                    notes.Rows[previousNotes.CurrentCell.RowIndex]["Note"] = noteBox.Text;
+
+                    using (SQLiteCommand command = new SQLiteCommand("UPDATE Notes SET Title=@title, Note=@note WHERE Title=@oldTitle", sqliteConn))
+                    {
+                        sqliteConn.Open();
+                        command.Parameters.AddWithValue("@title", titleBox.Text);
+                        command.Parameters.AddWithValue("@note", noteBox.Text);
+                        command.Parameters.AddWithValue("@oldTitle", notes.Rows[previousNotes.CurrentCell.RowIndex]["Title"]);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+                    notes.Rows.Add(titleBox.Text, noteBox.Text);
+
+                    using (SQLiteCommand command = new SQLiteCommand("INSERT INTO Notes (Title, Note) VALUES (@title, @note)", sqliteConn))
+                    {
+                        sqliteConn.Open();
+                        command.Parameters.AddWithValue("@title", titleBox.Text);
+                        command.Parameters.AddWithValue("@note", noteBox.Text);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                titleBox.Text = "";
+                noteBox.Text = "";
+                editing = false;
+            }
+            finally {
+                sqliteConn.Close();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void titleBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void previousNotes_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            titleBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilterNotes(string filterText)
+        {
+            notes.DefaultView.RowFilter = $"Title LIKE '%{filterText}%'";
+        }
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            FilterNotes(searchBox.Text);
+
+            previousNotes.DataSource = notes.DefaultView.ToTable();
+        }
+    }
+}
